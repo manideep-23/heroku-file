bbd7bba4c667b23b950791f33990b98c478b7986
manideep-23 <54998967+manideep-23@users.noreply.github.com>
2020-06-05 15:59:34 +0530

    Update tm.php

--- tm.php
+++ tm.php
@@ -1,3 +1,57 @@
 <?php
-echo "hii";
+
+
+$botToken = "1264179630:AAF_DNfcJVOSV2_c_fguu6viJScqaWNm7r4";
+	$website = "https://api.telegram.org/bot".$botToken;
+$web = "https://api.telegram.org/file/bot".$botToken;
+
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
+
+$chatID = $update['message']['from']['id'];
+$name = $update['message']['from']['first_name'];
+$text = $update['message']['text'];
+
+
+
+$agg = json_encode($update,JSON_PRETTY_PRINT);
+
+
+ 
+
+
+
+switch($text)
+{
+		case"/start":
+		sendMessage($chatID,"hi $name");
+		break;
+		case"/hello":
+		sendMessage($chatID,"hello");
+		break;
+		default:
+		
+		
+
+}
+
+
+
+
+
+		
+
+
+
+function sendMessage($chatID,$text)
+{
+	
+	$url = $GLOBALS[website]."/sendMessage?chat_id=$chatID&text=".urlencode($text);
+	file_get_contents($url);
+}
+
+
+
+
+
 ?>
