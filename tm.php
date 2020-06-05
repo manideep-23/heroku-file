aca52a0cbef5772d838c613460152850162e1b3f
manideep-23 <54998967+manideep-23@users.noreply.github.com>
2020-06-05 14:36:00 +0530

    Update tm.php

--- tm.php
+++ tm.php
@@ -1,8 +1,3 @@
-<html>
-<body>
-<form method="post" action="time2.php">
-Time:<input type="text" name="time" placeholder="enter in minutes"><br>
-<input type="submit">
-</form>
-</body>
-</html>
+<?php
+echo "hii";
+?>
